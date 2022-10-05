8ef7a306362c6b20c0b9a8969b1b36bfdc1fb325
LucasAlmeida-jpg <lucasalmeidagouvea123@gmail.com>
2022-10-05 10:25:17 -0300

    ajustes

--- all.js
+++ all.js
@@ -25,6 +25,7 @@ function showCreatorCalculator() {
   }
 }
 
+
 var filtroTeclas = function(event) {
   return ((event.charCode >= 48 && event.charCode <= 57) || (event.keyCode == 45 || event.charCode == 46))
 }
