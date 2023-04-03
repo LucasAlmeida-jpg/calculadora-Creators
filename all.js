93e9955efde12f7d0f33ffd0da9e0ae65860b2fc
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2023-04-03 13:27:24 -0300

    implementacao em andamento

--- all.js
+++ all.js
@@ -1,4 +1,3 @@
-
 const app = Vue.createApp({
   data() {
     return {
@@ -21,45 +20,100 @@ const app = Vue.createApp({
       warningMessage: '',
       redes: [
         {
-          name: "Youtube", icon: "fa-youtube", selected: false, visible: true,
+          name: 'Youtube',
+          icon: 'fa-youtube',
+          selected: false,
+          visible: true,
           contents: [
-            { name: "Shorts", select: false, socialValue: [1000, 4500, 6500, 9500, 12500, 18000, 18000] },
-            { name: "Youtube", select: false, socialValue: [1000, 5000, 15000, 25000, 35000, 40000, 50000] }
-          ], selectedTypes: [],
-          selectedIdx: null
-        },
-        {
-          name: "Instagram", icon: "fa-instagram", selected: false, visible: true,
-          contents: [
-            { name: "Feed", select: false, socialValue: [1000, 2500, 5500, 9000, 20000, 30000, 35000] },
-            { name: "Reels", select: false, socialValue: [1500, 5000, 8000, 14000, 25000, 35000, 45000] },
-            { name: "Stories", select: false, socialValue: [1000, 2500, 4000, 7000, 14000, 15000, 25000] },
+            {
+              name: 'Shorts',
+              select: false,
+              socialValue: [1000, 4500, 6500, 9500, 12500, 18000, 18000]
+            },
+            {
+              name: 'Youtube',
+              select: false,
+              socialValue: [1000, 5000, 15000, 25000, 35000, 40000, 50000]
+            }
           ],
           selectedTypes: [],
           selectedIdx: null
         },
         {
-          name: "TikTok", icon: "fa-tiktok", selected: false, visible: true,
+          name: 'Instagram',
+          icon: 'fa-instagram',
+          selected: false,
+          visible: true,
           contents: [
-            { name: "Vídeo", select: false, socialValue: [1500, 5000, 10000, 15000, 30000, 35000, 45000] }
-          ], selectedTypes: [],
+            {
+              name: 'Feed',
+              select: false,
+              socialValue: [1000, 2500, 5500, 9000, 20000, 30000, 35000]
+            },
+            {
+              name: 'Reels',
+              select: false,
+              socialValue: [1500, 5000, 8000, 14000, 25000, 35000, 45000]
+            },
+            {
+              name: 'Stories',
+              select: false,
+              socialValue: [1000, 2500, 4000, 7000, 14000, 15000, 25000]
+            }
+          ],
+          selectedTypes: [],
           selectedIdx: null
         },
         {
-          name: "Twitter", icon: "fa-twitter", selected: false, visible: true,
+          name: 'TikTok',
+          icon: 'fa-tiktok',
+          selected: false,
+          visible: true,
           contents: [
-            { name: "Twitter", select: false, socialValue: [1000, 2000, 7000, 10000, 15000, 15000, 20000] }
-          ], selectedTypes: [],
+            {
+              name: 'Vídeo',
+              select: false,
+              socialValue: [1500, 5000, 10000, 15000, 30000, 35000, 45000]
+            }
+          ],
+          selectedTypes: [],
           selectedIdx: null
         },
         {
-          name: "LinkedIn", icon: "fa-linkedin", selected: false, visible: false,
+          name: 'Twitter',
+          icon: 'fa-twitter',
+          selected: false,
+          visible: true,
           contents: [
-            { name: "Feed", select: false, socialValue: [1000, 5000, 5500, 80000, 11000, 15000, 15000] },
-            { name: "Vídeo", select: false, socialValue: [1000, 4500, 6500, 95000, 12500, 18000, 18000] },
-          ], selectedTypes: [],
+            {
+              name: 'Twitter',
+              select: false,
+              socialValue: [1000, 2000, 7000, 10000, 15000, 15000, 20000]
+            }
+          ],
+          selectedTypes: [],
           selectedIdx: null
         },
+        {
+          name: 'LinkedIn',
+          icon: 'fa-linkedin',
+          selected: false,
+          visible: false,
+          contents: [
+            {
+              name: 'Feed',
+              select: false,
+              socialValue: [1000, 5000, 5500, 80000, 11000, 15000, 15000]
+            },
+            {
+              name: 'Vídeo',
+              select: false,
+              socialValue: [1000, 4500, 6500, 95000, 12500, 18000, 18000]
+            }
+          ],
+          selectedTypes: [],
+          selectedIdx: null
+        }
         // {
         //   name: "Newsletter", icon: "fa fa-file-o", selected: false, visible: false, contents: [{name:"Texto", select:false}, {name:"Imagem",select:false}], selectedTypes: [],
         // },
@@ -86,115 +140,133 @@ const app = Vue.createApp({
       size: 0,
       posts: null,
       descriptions: [
-        "1K - 10K",
-        "11K - 50K",
-        "51K - 100K",
-        "101K - 500K",
-        "501K - 1M",
-        "1,1M - 10M",
-        "Super - 10M+",
-      ],
+        '1K - 10K',
+        '11K - 50K',
+        '51K - 100K',
+        '101K - 500K',
+        '501K - 1M',
+        '1,1M - 10M',
+        'Super - 10M+'
+      ]
     }
   },
 
   computed: {
     estimate() {
-      let value = 0;
-      const { size, posts, redes } = this;
+      let value = 0
+      const { size, posts, redes } = this
       for (const rede of redes) {
         if (rede.selected) {
           for (const content of rede.contents) {
             if (content.select && content.socialValue?.[size]) {
-              value += content.socialValue[size] * posts;
+              value += content.socialValue[size] * posts
             }
           }
         }
       }
 
-      this.valueEstimated = value;
-      return value;
+      this.valueEstimated = value
+      return value
     }
-
   },
 
   methods: {
     toggleCalculator() {
-      let hasSelected = false;
-      let hasSelectedFormat = false;
-    
-      this.redes.forEach((rede) => {
+      let hasSelected = false
+      let hasSelectedFormat = false
+
+      this.redes.forEach(rede => {
         if (rede.selected) {
-          hasSelected = true;
-          if (rede.contents.some((format) => format.select)) {
-            hasSelectedFormat = true;
+          hasSelected = true
+          if (rede.contents.some(format => format.select)) {
+            hasSelectedFormat = true
           }
         }
-      });
-    
+      })
+
       if (!hasSelected) {
-        this.warningMessage = 'Selecione a rede social';
-        this.showCalculator = false;
+        this.warningMessage = 'Selecione a rede social'
+        this.showCalculator = false
       } else if (!hasSelectedFormat) {
-        this.warningMessage = 'Selecione um formato dentro da rede social';
-        this.showCalculator = false;
+        this.warningMessage = 'Selecione um formato dentro da rede social'
+        this.showCalculator = false
       } else if (this.posts <= 0 || this.posts > 100) {
-        this.warningMessage = this.posts <= 0
-          ? 'É necessário digitar um número de posts'
-          : 'Número máximo de posts é de 100';
-        this.showCalculator = false;
+        this.warningMessage =
+          this.posts <= 0
+            ? 'É necessário digitar um número de posts'
+            : 'Número máximo de posts é de 100'
+        this.showCalculator = false
       } else {
-        this.showCalculator = true;
-        this.warningMessage = '';
+        this.showCalculator = true
+        this.warningMessage = ''
       }
+    },
+
+    selectNetwork(selected) {
+      this.redes.forEach(network => {
+        network.contents.forEach(content => {
+          if (content.name === selected) {
+            content.select = true;
+            network.selected = true;
+          }
+        });
+      });
     },
     
     toggleItens(visible) {
       for (let i = 4; i < this.redes.length; i++) {
-        this.redes[i].visible = visible;
+        this.redes[i].visible = visible
       }
     },
 
     socialName(nome, contents, selected) {
-      this.socialSelected = nome;
-      this.typeSelected = contents;
-      this.selectedTypes = selected;
+      this.socialSelected = nome
+      this.typeSelected = contents
+      this.selectedTypes = selected
     },
 
     confirmSelection(send) {
       let rede = this.redes
-      let index = rede.findIndex(obj => obj.name === send);
+      let index = rede.findIndex(obj => obj.name === send)
       this.selectedTypes.forEach(function (elemento, indice) {
         rede[index].contents[indice].select = true
-      });
-      rede[index].selectedTypes = this.selectedTypes; 
-      rede[index].selectedIdx = index;
-      this.redes = rede;
+      })
+      rede[index].selectedTypes = this.selectedTypes
+      rede[index].selectedIdx = index
+      this.redes = rede
     },
 
     unselectAll(send) {
       let rede = this.redes
-      let index = rede.findIndex(obj => obj.name === send);
+      let index = rede.findIndex(obj => obj.name === send)
       this.selectedTypes.forEach(function (elemento, indice) {
         rede[index].contents[indice].select = false
-      });
-      rede[index].selectedTypes = []; 
-      rede[index].selectedIdx = index;
-      this.redes = rede;
-      this.selectedTypes = [];
+      })
+      rede[index].selectedTypes = []
+      rede[index].selectedIdx = index
+      this.redes = rede
+      this.selectedTypes = []
     },
 
     selectedCountNotification(rede) {
       if (rede && rede.selectedTypes) {
-        return rede.selectedTypes.filter(index => index).length;
+        return rede.selectedTypes.filter(index => index).length
       } else {
-        return 0;
+        return 0
       }
     },
 
     sendSimulation() {
-      var selectedsNetworks = this.redes.filter(network => network.selected === true).map(network => {
-        return { name: network.name, contents: network.contents.filter(content => content.select === true).map(content => content.name) }
-      });
+      var selectedsNetworks = this.redes
+        .filter(network => network.selected === true)
+        .map(network => {
+          return {
+            name: network.name,
+            contents: network.contents
+              .filter(content => content.select === true)
+              .map(content => content.name)
+          }
+        })
 
       var data = {
         size: this.descriptions[this.size],
@@ -209,12 +281,10 @@ const app = Vue.createApp({
         method: 'post',
         url: 'https://creators.llc/api/v1/calculator/send-simulation',
         data: data
-      });
-
+      })
     }
   }
-
-}).mount('#calc');
+}).mount('#calc')
 
 var rellax = new Rellax('.rellax', {
   speed: -0.7,
@@ -223,4 +293,4 @@ var rellax = new Rellax('.rellax', {
   round: true,
   vertical: true,
   horizontal: true
-});
+})
